4780872393f90dda1cdd14723bcfef90320cd3ac
SHANMUKHAGIRADA <220211073+SHANMUKHAGIRADA@users.noreply.github.com>
2026-06-28 21:32:55 +0530

    Solve mean-squared-error

--- mean-squared-error/mean-squared-error.py
+++ mean-squared-error/mean-squared-error.py
@@ -1,12 +1,7 @@
 import numpy as np
 
 def mean_squared_error(y_pred, y_true):
-    n=len(y_true)
-    sum=0;
-    for i in range(n):
-        sum+=(y_pred[i]-y_true[i])**2
-    return (1/n)*(sum)
+    y_pred = np.array(y_pred)
+    y_true = np.array(y_true)
 
-    
-    
-    
+    return np.mean((y_pred - y_true) ** 2)
